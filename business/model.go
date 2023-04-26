9ed842f15ed2b0867c99690dc8aecd804b993236
kneerunjun <kneerunjun@gmail.com>
2023-04-26 16:08:27 +0000

    restarting design from business logic all over

---
+++ business/model.go
@@ -0,0 +1,70 @@
+package business
+
+import (
+	"fmt"
+	"time"
+)
+
+/* ==================================
+author 		: kneerunjun@gmail.com
+time		: April 2023
+project		: botmincock
+Account objects that face the database are required
+Account objects represent objects that store the basic facts about the account
+For all other collections Account info serves as the base for getting unique ids
++ CRUD functions for the account
+====================================*/
+
+type AccElev uint8
+
+// Account elevation as enumeration
+const (
+	User = uint8(0) + iota
+	Manager
+	Admin
+)
+
+// UserAccount : this has the basic user account facts
+// refer to this to know the details of the user
+// NOTE: we arent using bson.ObjectIds here since telegID is sufficent Primary key
+type UserAccount struct {
+	TelegID  int64   `bson:"tid" json:"tid"`         // telegram chat id for personal conversations, unique
+	Email    string  `bson:"email" json:"email"`     // email of the user, for reports, unique
+	Name     string  `bson:"name" json:"name"`       // name for addressing the user in any conversation, unique
+	Elevtn   AccElev `bson:"elevtn" json:"elevtn"`   // priveleges for the user account 0-user, 1-manager, 2-admin, from specified enumerated values only
+	Archived bool    `bson:"archive" json:"archive"` // default setting is false here
+}
+
+func (ua *UserAccount) ToMsgTxt() string {
+	// https://stackoverflow.com/questions/3871729/transmitting-newline-character-n
+	return fmt.Sprintf("Hi,%s%%0AYou are registered with us%%0AEmail: %s%%0ATelegramID: %d", ua.Name, ua.Email, ua.TelegID)
+}
+
+// Transac :towards account maintenance - each row is a credit / debit attributed to the account
+// denormalized this has to be aggregated to see the balance of the account
+type Transac struct {
+	TelegID int64     `bson:"tid" json:"tid"`
+	Credit  int       `bson:"credit" json:"credit"`
+	Debit   int       `bson:"debit" json:"debit"`
+	Desc    string    `bson:"desc" json:"desc"`
+	DtTm    time.Time `bson:"date" json:"date"`
+}
+
+// Start of each month the playdays are estimated from polls in the group
+// each month - each acccount the play days help you arrive at the share holding of the account in the monthly expenditure
+// Estimates are pivotal to calculating the monthly paybacks or dues
+type Estimate struct {
+	TelegID int64  `bson:"tid" json:"tid"`
+	PlyDys  uint8  `bson:"plydys" json:"plydys"`
+	Month   uint8  `bson:"mnth" json:"mnth"`
+	Year    uint16 `bson:"yr" json:"yr"`
+}
+
+// Any purchases on behalf of the bot as a manager by any account towards goods/services shared by the group is recorded as an expense
+type Expense struct {
+	TelegID int64  `bson:"tid" json:"tid"`
+	Month   uint8  `bson:"mnth" json:"mnth"`
+	Year    uint16 `bson:"yr" json:"yr"`
+	Desc    string `bson:"desc" json:"desc"`
+	INR     int    `bson:"inr" json:"inr"`
+}
---
+++ business/ua.go
@@ -0,0 +1,114 @@
+package business
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+)
+
+var (
+	REGX_EMAIL = regexp.MustCompile("^$")
+)
+
+type DbAdaptor interface {
+	AddOne(interface{}) error
+	RemoveOne(interface{}) error
+	UpdateOne(interface{}) error
+	GetOne(interface{}, reflect.Type) (interface{}, error)
+	GetCount(interface{}, *int) error
+}
+
+func valid_account(ua *UserAccount) bool {
+	return true
+}
+
+func duplicate_account(ua *UserAccount) bool {
+	return false
+}
+
+// RegisterNewAccount: checks for the account duplicates, validates the account values and then just pushes that to the database
+// Any account when registered new will be marked archived = false
+// any account when registered new will have elevation =0
+// Icnase of already registered account but archived its re-enabled
+func RegisterNewAccount(ua *UserAccount, iadp DbAdaptor) error {
+	if !valid_account(ua) {
+		return fmt.Errorf("account %s isnt a valid account", ua.Email)
+	}
+	duplicate := 0
+	if err := iadp.GetCount(&UserAccount{TelegID: ua.TelegID}, &duplicate); err != nil {
+		return fmt.Errorf("error checking for account duplicate %d: %s", ua.TelegID, err)
+	}
+	if duplicate != 0 {
+		return fmt.Errorf("account with id %d already registered", ua.TelegID)
+	}
+	archived := 0
+	if err := iadp.GetCount(&UserAccount{TelegID: ua.TelegID, Archived: true}, &archived); err != nil {
+		return fmt.Errorf("failed to get count of similar accounts but archived %d: %s", ua.TelegID, err)
+	}
+	if archived != 0 {
+		// this means the account just needs re-enablement and not registration
+		// this happens with updating the email id as well.
+		if err := iadp.UpdateOne(&UserAccount{TelegID: ua.TelegID, Archived: false, Email: ua.Email}); err != nil {
+			return fmt.Errorf("failed to re-enabled the account %d", ua.TelegID)
+		}
+		return fmt.Errorf("user account %d was found already registered, but was archived. Re-enabled it", ua.TelegID)
+	}
+	// defaults when registering new account
+	ua.Elevtn = AccElev(User)
+	ua.Archived = false
+	if err := iadp.AddOne(ua); err != nil {
+		return fmt.Errorf("failed query to add account %s %d: %s", ua.Email, ua.TelegID, err)
+	}
+	return nil
+}
+
+// ElevateAccount : comes in handy when an account has to be promoted in role
+func ElevateAccount(ua *UserAccount, iadp DbAdaptor) error {
+	if !duplicate_account(ua) {
+		return fmt.Errorf("Account %s %d could not be found registered", ua.Email, ua.TelegID)
+	}
+	if ua.Elevtn < AccElev(User) && ua.Elevtn > AccElev(Admin) {
+		// has to be between permissible limits
+		return fmt.Errorf("Requested account elevation is invalid, Has to be between [%d,%d]", User, Admin)
+	}
+	if err := iadp.UpdateOne(&UserAccount{TelegID: ua.TelegID, Elevtn: ua.Elevtn}); err != nil {
+		return fmt.Errorf("failed query to update  account %d: %s", ua.TelegID, err)
+	}
+	updated, err := iadp.GetOne(&UserAccount{TelegID: ua.TelegID}, reflect.TypeOf(&UserAccount{}))
+	if err != nil {
+		return fmt.Errorf("failed query to get account %d: %s", ua.TelegID, err)
+	}
+	x, _ := updated.(*UserAccount)
+	*ua = *x
+	return nil
+}
+
+func UpdateAccountEmail(ua *UserAccount, iadp DbAdaptor) error {
+	if !duplicate_account(ua) {
+		return fmt.Errorf("account %s %d could not be found registered", ua.Email, ua.TelegID)
+	}
+	if ua.Email == "" || !REGX_EMAIL.MatchString(ua.Email) {
+		// has to be between permissible limits
+		return fmt.Errorf("requested account email is invalid")
+	}
+	if err := iadp.UpdateOne(&UserAccount{TelegID: ua.TelegID, Email: ua.Email}); err != nil {
+		return fmt.Errorf("failed query to update  account %d: %s", ua.TelegID, err)
+	}
+	updated, err := iadp.GetOne(&UserAccount{TelegID: ua.TelegID}, reflect.TypeOf(&UserAccount{}))
+	if err != nil {
+		return fmt.Errorf("failed query to get account %d: %s", ua.TelegID, err)
+	}
+	x, _ := updated.(*UserAccount)
+	*ua = *x
+	return nil
+}
+
+func DeregisterAccount(ua *UserAccount, iadp DbAdaptor) error {
+	if !duplicate_account(ua) {
+		return fmt.Errorf("account %d could not be found registered", ua.TelegID)
+	}
+	if err := iadp.UpdateOne(&UserAccount{TelegID: ua.TelegID, Archived: true}); err != nil {
+		return fmt.Errorf("failed query to update  account %d: %s", ua.TelegID, err)
+	}
+	return nil
+}
