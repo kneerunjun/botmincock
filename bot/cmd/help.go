32d776a6006966183b891442495a3dab9192bdfa
Niranjan Awati <kneerunjun@gmail.com>
2023-05-10 04:29:26 +0530

    #24 emoticons for the help response

--- bot/cmd/help.go
+++ bot/cmd/help.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/kneerunjun/botmincock/bot/resp"
 )
 
@@ -9,7 +11,7 @@ type HelpBotCmd struct {
 }
 
 var (
-	ALL_HELP string = "Welcome to botmincock%0AHere we enlist all the commands that are available.%0A@psabadminton_bot /registerme <email>%0A@psabadminton_bot /editme <email>%0A@psabadminton_bot /myinfo%0A@psabadminton_bot /deregisterme%0A@psabadminton_bot /elevateacc <telegramid>%0A@psabadminton_bot /addexpense <INR> <remarks>%0A@psabadminton_bot /myexpense%0A@psabadminton_bot /allexpenses"
+	ALL_HELP string = fmt.Sprintf("%c Botmincock v0.0.0 %cPSA Badminton Team %c-%c%%0A%%0ACommands:%%0A@psabadminton_bot /registerme <email>%%0A@psabadminton_bot /editme <new-email>%%0A@psabadminton_bot /myinfo%%0A@psabadminton_bot /deregisterme%%0A@psabadminton_bot /elevateacc <TelegramID>%%0A@psabadminton_bot /addexpense <INR> <remarks>%%0A@psabadminton_bot /myexpense%%0A@psabadminton_bot /allexpenses", resp.EMOJI_robot, resp.EMOJI_copyrt, resp.EMOJI_banana, resp.EMOJI_garlic)
 )
 
 func (info *HelpBotCmd) Execute(ctx *CmdExecCtx) resp.BotResponse {
--- bot/resp/itf.go
+++ bot/resp/itf.go
@@ -1,5 +1,27 @@
 package resp
 
+import (
+	"strconv"
+	"strings"
+)
+
+var (
+	// https://unicode.org/emoji/charts/full-emoji-list.html
+	EMOJI_warning, _   = strconv.ParseInt(strings.TrimPrefix("\\U26A0", "\\U"), 16, 32)
+	EMOJI_grinface, _  = strconv.ParseInt(strings.TrimPrefix("\\U1F600", "\\U"), 16, 32)
+	EMOJI_rofl, _      = strconv.ParseInt(strings.TrimPrefix("\\U1F923", "\\U"), 16, 32)
+	EMOJI_redcross, _  = strconv.ParseInt(strings.TrimPrefix("\\U274C", "\\U"), 16, 32)
+	EMOJI_redqs, _     = strconv.ParseInt(strings.TrimPrefix("\\U2753 	", "\\U"), 16, 32)
+	EMOJI_bikini, _    = strconv.ParseInt(strings.TrimPrefix("\\U1F459", "\\U"), 16, 32)
+	EMOJI_greentick, _ = strconv.ParseInt(strings.TrimPrefix("\\U2705", "\\U"), 16, 32)
+	EMOJI_clover, _    = strconv.ParseInt(strings.TrimPrefix("\\U1F340", "\\U"), 16, 32)
+	EMOJI_meat, _      = strconv.ParseInt(strings.TrimPrefix("\\U1F357", "\\U"), 16, 32)
+	EMOJI_robot, _     = strconv.ParseInt(strings.TrimPrefix("\\U1F916", "\\U"), 16, 32)
+	EMOJI_copyrt, _    = strconv.ParseInt(strings.TrimPrefix("\\U00A9", "\\U"), 16, 32)
+	EMOJI_banana, _    = strconv.ParseInt(strings.TrimPrefix("\\U1F34C", "\\U"), 16, 32)
+	EMOJI_garlic, _    = strconv.ParseInt(strings.TrimPrefix("\\U1F9C4", "\\U"), 16, 32)
+)
+
 type BotResponse interface {
 	UserMessage() string
 	SendMsgUrl() string // url over which the bot response would be sent
