6bf12aad406916656075ffa2142df74e02abc932
Niranjan <kneerunjun@gmail.com>
2023-05-24 06:25:37 +0530

    #28 PlayerShare implementation ready for testing

--- biz/estimates.go
+++ biz/estimates.go
@@ -1,10 +1,91 @@
 package biz
 
-import "github.com/kneerunjun/botmincock/dbadp"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/kneerunjun/botmincock/dbadp"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
 // PlayerShare : for any player that has indicated his efforts estimate, this will get share of his contribution for a given month
-// est 		:
-func PlayerShare(est *Estimate, iadp dbadp.DbAdaptor, result float32) error {
+// tID 		: share of the estimates any player for the current month
+// this assumes you have valid data from the poll else error
 
-	return nil
+func PlayerShare(tID int64, iadp dbadp.DbAdaptor) (float32, error) {
+	errLoc := "PlayerShare"
+	result := struct {
+		Total int `bson:"total"`
+	}{}
+	totalPlayDays := func() int {
+		// this where we make calls to the database
+		from, to := MonthAsBoundary()
+		err := iadp.Aggregate([]bson.M{
+			{"$match": bson.M{
+				"dttm": bson.M{
+					"$gte": from,
+					"$lte": to,
+				},
+			}},
+			{"$group": bson.M{
+				"_id": nil,
+				"total": bson.M{
+					"$sum": "$plydys",
+				},
+			}},
+			{"$project": bson.M{
+				"_id": 0,
+			}},
+		}, &result)
+		if err != nil {
+			if errors.Is(err, mgo.ErrNotFound) {
+				return 0
+			}
+			return -1
+		}
+		return result.Total
+	}
+	tpd := totalPlayDays()
+	if tpd < 0 {
+		return 0.0, NewDomainError(fmt.Errorf("failed to get total play days"), nil).SetLoc(errLoc).SetUsrMsg("Wasnt possible to get the estimated playdays, please report this to an admin").SetLogEntry(logrus.Fields{
+			"total_play_days": tpd,
+		})
+	}
+	myPlayDays := func(tID int64) int {
+		from, to := MonthAsBoundary()
+		err := iadp.Aggregate([]bson.M{
+			{"$match": bson.M{
+				"dttm": bson.M{
+					"$gte": from,
+					"$lte": to,
+				},
+				"tid": tID, // this will get us only the player playdays
+			}},
+			{"$group": bson.M{
+				"_id": nil,
+				"total": bson.M{
+					"$sum": "$plydys",
+				},
+			}},
+			{"$project": bson.M{
+				"_id": 0,
+			}},
+		}, &result)
+		if err != nil {
+			if errors.Is(err, mgo.ErrNotFound) {
+				return 0
+			}
+			return -1
+		}
+		return result.Total
+	}
+	mpd := myPlayDays(tID)
+	if mpd < 0 {
+		return 0.0, NewDomainError(fmt.Errorf("failed to get player play days"), nil).SetLoc(errLoc).SetUsrMsg("Wasnt possible to get the estimated playdays, please report this to an admin").SetLogEntry(logrus.Fields{
+			"my_play_days": mpd,
+		})
+	}
+	return float32(mpd) / float32(tpd), nil
 }
